c49a31ccdaef78c7f62585731f966b8abe881631
李绪杰 <1340482172@qq.com>
2022-05-14 12:57:04 +0800

    头部搭建完成

---
+++ src/components/MyHeader/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <a class="navbar-brand header-title">文章博客</a>
+      <ul class="nav navbar-nav pull-xs-right header-right">
+        <li class="nav-item">
+          <a class="nav-link active" href="">主页</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">
+            <i class="ion-compose"></i>&nbsp;创建文章
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">
+            <i class="ion-gear-a"></i>&nbsp;我的资料
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">登录</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="">注册</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="less" scoped>
+a {
+  color: gray;
+}
+a.active {
+  color: #000;
+}
+.header-title {
+  float: left;
+  font-weight: bold;
+  font-size: 24px;
+  color: #5cb85c;
+}
+
+.header-right {
+  float: right;
+}
+</style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <my-header></my-header>
+    <my-container></my-container>
+  </div>
+</template>
+
+<script>
+import MyHeader from '@/components/MyHeader'
+import MyContainer from '@/components/MyContainer'
+export default {
+  name: 'Home',
+  components: { MyHeader, MyContainer }
+}
+</script>
+
+<style></style>
